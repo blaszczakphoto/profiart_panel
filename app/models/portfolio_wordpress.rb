681acf3decae3f98b5043ddb613d4de5f919ff87
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-05-15 09:21:31 +0200

    order wp portfolios by id

--- app/models/portfolio_wordpress.rb
+++ app/models/portfolio_wordpress.rb
@@ -3,12 +3,12 @@ class PortfolioWordpress < ApplicationRecord
 	self.table_name = 'profiart_wp5.wp_posts'
 
   def self.all_portfolio
-    PortfolioWordpress.where(post_type: 'portfolio')
+    PortfolioWordpress.where(post_type: 'portfolio').order(:ID)
   end
 
   def self.names_with_id
     self.all_portfolio.map do |post|
-      "#{post.post_title}##{post.id}"
+      "#{post.post_title} - ID##{post.id}"
     end
   end
 end
